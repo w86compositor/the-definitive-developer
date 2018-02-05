26e9e3d69406687dee8b7199a3a754912bc6565a
Waldo Lavaut <w86compositor@gmail.com>
2018-02-04 21:39:54 -0500

    added the device variant to the application controller'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :detect_device_variant
+
+  private
+  def detect_device_variant
+    request.variant = :tablet if browser.device.tablet?
+    request.variant = :mobile if browser.device.mobile?
+    request.variant = :desktop if !browser.device.mobile? && !browser.device.tablet?
+  end
 end
