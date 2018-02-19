740072203fe301f5acd53ca612f6fff9687cc24e
Waldo Lavaut <w86compositor@gmail.com>
2018-02-18 22:26:26 -0500

    Still going crazy...keep coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'about_me/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
   root 'home#index'
 end
